3f6bf6cd6754004f6f923fa591f89a2fa3480c8f
lyneca <lukemtuthill@gmail.com>
2017-10-10 10:57:34 +1100

    added .config dirs

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,17 @@
+# Basic script to kill all old bars and launch new.
+
+# Terminate already running bad instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+# while grep -x polybar >/dev/null; do sleep 1; done
+
+# Launch the example bar
+xrandr | grep -q "HDMI-1 connected 1"
+
+if [ $? -eq 0 ]; then
+	MONITOR=HDMI-1 polybar -q top &
+	MONITOR=HDMI-1 polybar -q bottom &
+fi
+MONITOR=eDP-1 polybar -q top &
+MONITOR=eDP-1 polybar -q bottom &
