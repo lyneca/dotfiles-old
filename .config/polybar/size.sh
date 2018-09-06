68beb71e3655ee22b4352937dd7b417d1ad35504
Luke <lukemtuthill@gmail.com>
2018-09-06 15:22:37 +1000

    Update polybar

--- .config/polybar/size.sh
+++ .config/polybar/size.sh
@@ -1,8 +1,8 @@
 if [ -e $1 ]; then
-    size=$(df $1 --output=used | tail -n 1 | tr -d ' ')
-    if [ $size -gt "$4000" ]; then
+    size=$(df $1 --output=pcent | tail -n 1 | tr -d ' ' | tr -d '%')
+    if [ $size -gt "$4" ]; then
         echo -n "%{F#ff5555}";
-    elif [ $size -gt "$3000" ]; then
+    elif [ $size -gt "$3" ]; then
         echo -n "%{F#fba922}";
     else
         echo -n "%{F#aaff77}";
