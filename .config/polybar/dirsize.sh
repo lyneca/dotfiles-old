9436d766d65db47bf360a62cae743bf2afef7baf
Luke Tuthill <lukemtuthill@gmail.com>
2018-09-11 22:37:53 +1000

    Update colors on polybar and add aux storage

--- .config/polybar/dirsize.sh
+++ .config/polybar/dirsize.sh
@@ -1,10 +1,12 @@
+source $HOME/.cache/wal/colors.sh
+
 size=$(du -d 0 $1 2>/dev/null | cut -f 1)
 if [ $size -gt "$4000" ]; then
-    echo -n "%{F#ff5555}";
+    echo -n "%{F$color6}";
 elif [ $size -gt "$3000" ]; then
-    echo -n "%{F#fba922}";
+    echo -n "%{F$color5}";
 else
-    echo -n "%{F#aaff77}";
+    echo -n "%{F$color1}";
 fi
 # echo "$(du -hd 0 $1 | cut -f 1) $2%{F-}";
 echo "$2%{F-}";
--- .config/polybar/size.sh
+++ .config/polybar/size.sh
@@ -1,11 +1,13 @@
+source $HOME/.cache/wal/colors.sh
+
 if [ -e $1 ]; then
     size=$(df $1 --output=pcent | tail -n 1 | tr -d ' ' | tr -d '%')
     if [ $size -gt "$4" ]; then
-        echo -n "%{F#ff5555}";
+        echo -n "%{F$color6}";
     elif [ $size -gt "$3" ]; then
-        echo -n "%{F#fba922}";
+        echo -n "%{F$color5}";
     else
-        echo -n "%{F#aaff77}";
+        echo -n "%{F$color1}";
     fi
     echo "$2%{F-}";
 fi
